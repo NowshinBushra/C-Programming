7d987fff6adad98e47ccf8148038d9488fcc3810
Nowshin Islam Bushra <74665436+NowshinBushra@users.noreply.github.com>
2024-08-23 21:59:42 +0600

    Add files via upload

---
+++ Module 9/copy two element.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main(){
+
+ int n;
+ scanf("%d", &n);
+
+ int a[n];
+    for(int i=0; i<n; i++){
+        scanf("%d", &a[i]);
+ }
+
+ int m;
+ scanf("%d", &m);
+
+ int b[m];
+    for(int j=0; j<m; j++){
+        scanf("%d", &b[j]);
+ }
+
+
+ int c[n+m];
+    for(int i=0; i<n; i++){
+        c[i]=a[i];
+ }
+
+ int i=n;
+    for(int j=0; j<m; j++){
+        c[i]=b[j];
+        i++;
+ }
+
+
+    for(int i=0; i<n+m; i++){
+        printf("%d ", c[i]);
+ }
+ return 0;
+}
